52f221d21e2b578649609670df82f057a526d976
davangcas <davangcas@gmail.com>
2021-09-04 14:42:27 -0300

    Debug to false

--- config/settings.py
+++ config/settings.py
@@ -142,9 +142,6 @@ if LOCAL_DEPLOY:
     ]
 else:
     if HEROKU_DEPLOY:
-        STATICFILES_DIRS = [
-            os.path.join(BASE_DIR, 'static'),
-        ]
         STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
         STATIC_URL = '/static/'
         STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
