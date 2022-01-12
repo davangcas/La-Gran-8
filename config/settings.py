776b89788eaea277bb1ce74d6b70b710831aa059
Angel David Castro <davangcas@gmail.com>
2022-01-12 13:29:23 -0300

    fix: revert whitenoise

--- config/settings.py
+++ config/settings.py
@@ -144,7 +144,7 @@ else:
     if HEROKU_DEPLOY:
         STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
         STATIC_URL = '/static/'
-        STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+        STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     else:
         STATICFILES_DIRS = [
             os.path.join(BASE_DIR, 'static'),
